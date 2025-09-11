cf2d5006b546392bc01af8b18e988fb30f44b2e1
Mansi Mandviya <95748400+mansimandviya@users.noreply.github.com>
2025-09-11 13:36:38 +0530

    Data insertion and testing endpoints

--- backend/model/HoldingsModel.js
+++ backend/model/HoldingsModel.js
@@ -1,6 +1,7 @@
-const {model} = require("mongoose");
-const { HoldingsSchema } =  require('../schemas/HoldingsSchema');
+const { model } = require("mongoose");
+
+const { HoldingsSchema } = require("../schemas/HoldingsSchema");
 
 const HoldingsModel = new model("holding", HoldingsSchema);
 
-module.exports = (HoldingsModel);
+module.exports = { HoldingsModel };
