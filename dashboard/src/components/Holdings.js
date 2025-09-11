c967b81d6372516dd14387d8bfb5be4fdf51d792
Mansi Mandviya <95748400+mansimandviya@users.noreply.github.com>
2025-09-11 14:55:51 +0530

    fetch data from dashboard frontend and backend

--- dashboard/src/components/Holdings.js
+++ dashboard/src/components/Holdings.js
@@ -1,10 +1,22 @@
-import React from "react";
-import { holdings } from "../data/Data";
+import React, { useState, useEffect } from "react";
+import axios, { all } from "axios";
+//  import { VerticalGraph } from "./VerticalGraph";
+
+// import { holdings } from "../data/data";
 
 const Holdings = () => {
+  const [allHoldings, setAllHoldings] = useState([]);
+
+  useEffect(() => {
+    axios.get("http://localhost:3002/allHoldings").then((res) => {
+      // console.log(res.data);
+      setAllHoldings(res.data);
+    });
+  }, []);
+
   return (
     <>
-      <h3 className="title">Holdings ({holdings.lengtth})</h3>
+      <h3 className="title">Holdings ({allHoldings.length})</h3>
 
       <div className="order-table">
         <table>
@@ -18,23 +30,27 @@ const Holdings = () => {
             <th>Net chg.</th>
             <th>Day chg.</th>
           </tr>
-          {holdings.map((stock, index) => {
+
+          {allHoldings.map((stock, index) => {
             const curValue = stock.price * stock.qty;
             const isProfit = curValue - stock.avg * stock.qty >= 0.0;
             const profClass = isProfit ? "profit" : "loss";
-            const dayClass = stock.isLoss ? "loss":"profit";
+            const dayClass = stock.isLoss ? "loss" : "profit";
 
-            return(
-            <tr key={index} >
-            <td>{stock.name}</td>
-            <td>{stock.qty}</td>
-            <td>{stock.avg.toFixed(2)}</td>
-            <td>{stock.price.toFixed(2)}</td>
-            <td>{curValue.toFixed(2)}</td>
-            <td className={profClass}>{(curValue-stock.avg*stock.qty).toFixed(2)}</td>
-            <td className={profClass}>{stock.net}</td>
-            <td className={dayClass}>{stock.day}</td>
-          </tr>)
+            return (
+              <tr key={index}>
+                <td>{stock.name}</td>
+                <td>{stock.qty}</td>
+                <td>{stock.avg.toFixed(2)}</td>
+                <td>{stock.price.toFixed(2)}</td>
+                <td>{curValue.toFixed(2)}</td>
+                <td className={profClass}>
+                  {(curValue - stock.avg * stock.qty).toFixed(2)}
+                </td>
+                <td className={profClass}>{stock.net}</td>
+                <td className={dayClass}>{stock.day}</td>
+              </tr>
+            );
           })}
         </table>
       </div>
